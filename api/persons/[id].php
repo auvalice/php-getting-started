b4a1f44795402c4fc52f7fe1f1fb2a06e0925da6
Alice Vee <auvalice.outlook.com>
2023-05-07 13:51:53 +0200

    JSON return example

--- api/persons/[id].php
+++ api/persons/[id].php
@@ -1,34 +1,21 @@
-<table>
+<?php
 
-    <?php
+require_once $_SERVER["DOCUMENT_ROOT"] . "/repositories/db.php";
 
-    require_once $_SERVER["DOCUMENT_ROOT"] . "/repositories/db.php";
+$conn = get_connection();
 
-    $conn = get_connection();
+$sql_result = $conn->execute_query("SELECT * FROM insurance_details WHERE ID = ?;", [$id]);
+$fields = $sql_result->fetch_fields();
+$count = sizeof($fields);
 
-    $result = $conn->execute_query("SELECT * FROM insurance_details WHERE ID = ?;", [$id]);
+$result = new stdClass();
 
-    $fields = $result->fetch_fields();
-
-    echo "<thead><tr>";
-
-    foreach ($fields as $field) {
-        echo "<th>" . $field->name . "</th>";
+while ($row = $sql_result->fetch_row()) {
+    for ($i = 0; $i < $count; $i++) {
+        $result->{$fields[$i]->name} = $row[$i];
     }
+}
 
-    echo "</tr></thead>";
-    echo "<tbody>";
+echo json_encode($result);
 
-    while ($row = $result->fetch_row()) {
-        echo "<tr>";
-        foreach ($row as $row_value) {
-            echo "<td>" . $row_value . "</td>";
-        }
-        echo "</tr>";
-    }
-
-    echo "</tbody>";
-    $conn->close();
-    ?>
-
-</table>
+$conn->close();
